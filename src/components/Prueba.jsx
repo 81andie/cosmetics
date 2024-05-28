42c8b6704bf35f32e9ae088957e208d4e7e289ff
81andie <133395577+81andie@users.noreply.github.com>
2024-05-28 21:12:54 +0200

    Llenando y maquetando la sección Ofertas Flash

--- src/components/Prueba.jsx
+++ src/components/Prueba.jsx
@@ -35,13 +35,14 @@ export const Prueba = () => {
     const itemTemplate = (product) => {
         return (
             <div className="col-12">
+                
                 <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
                     <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round img-products" src={`images/${product.image}`} alt={product.name} />
                     <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
                         <div className="flex flex-column align-items-center sm:align-items-start gap-3">
                             <div className="text-2xl font-bold text-900">{product.name}</div>
                             <Rating value={product.rating} readOnly cancel={false}></Rating>
-                            <span className="font-semibold">{product.description}</span>
+                            <span className="font-semibold sm:align-items-start ">{product.description}</span>
                             <div className="flex align-items-left gap-3">
                                 <span className="flex align-items-center gap-2">
                                     <i className="pi pi-tag"></i>
@@ -54,12 +55,14 @@ export const Prueba = () => {
                             </div>
                         </div>
                         <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                            <span className="text-2xl font-semibold">${product.price}</span>
+                            <span className="text-2xl font-semibold --primary-700">${product.price}</span>
                             <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
                         </div>
                     </div>
                 </div>
+                
             </div>
+        
         );
     };
 
