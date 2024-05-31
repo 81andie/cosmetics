f39deefd585c694b0d3459fa5acb429fe5103946
81andie <133395577+81andie@users.noreply.github.com>
2024-05-31 22:36:58 +0200

    Cambios en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,13 @@
 import React from 'react'
 import { Navbar } from './Navbar';
 import { useState, useEffect } from 'react';
-import { Galleria } from 'primereact/galleria';
-import { PhotoService } from '../photoservice/Photoservice';
+
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 export const Header = () => {
-    const [images, setImages] = useState(null);
-    const responsiveOptions = [
-        {
-            breakpoint: '400px',
-            numVisible: 3
-        },
-        {
-            breakpoint: '400px',
-            numVisible: 3
-        },
-        {
-            breakpoint: '400px',
-            numVisible: 2
-        }
-    ];
-
-    useEffect(() => {
-        PhotoService.getImages().then(data => setImages(data));
-    }, [])
-
-    const itemTemplate = (item) => {
-        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%' }} />
-    }
-
-    const thumbnailTemplate = (item) => {
-        return <img src={item.thumbnailImageSrc} alt={item.alt} />
-    }
+    
 
 
 
@@ -55,10 +28,7 @@ export const Header = () => {
         <img src="/logo.jpg" className="logo"/>
       
         
-        <div className="card">
-            <Galleria value={images} responsiveOptions={responsiveOptions} numVisible={3} style={{ maxWidth: '640px' }} 
-                item={itemTemplate} thumbnail={thumbnailTemplate} />
-        </div>
+       
     
        
     </header>
