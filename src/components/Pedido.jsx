0b7e0f51f28f90de18920f4502deee14e2afbedd
81andie <133395577+81andie@users.noreply.github.com>
2024-06-04 15:19:03 +0200

    Configurando el localStorage para ir sacando el pedido y renderizarlo

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,85 +1,95 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { ProductServiceMarca } from '../photoservice/ProductServiceMarca';
 
 export const Pedido = () => {
 
-  const[cart, setCart]= useState([]);
+  const [cart, setCart] = useState([]);
 
-const getCartFromLocalStorage=()=>{
-  const savedCart = localStorage.getItem('cart');
-  return savedCart ? JSON.parse(savedCart): [];
-}
+  const getCartFromLocalStorage = () => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  }
 
-useEffect(()=>{
-  const cartFromLocalStorage = getCartFromLocalStorage();
-})
+  useEffect(() => {
+    const cartFromLocalStorage = getCartFromLocalStorage();
+    if (cartFromLocalStorage) {
+      setCart(cartFromLocalStorage);
+
+      console.log(cart);
+
+
+    }
+  }, []);
 
 
 
 
-    return (
-        <section className="container_pedido">
-            <div className="pedido_titulo">
-                <img src="/logo.jpg" className="logo_tienda1" />
-                <h1>TU PEDIDO</h1> 
-              
-            </div>
-            <hr></hr>
 
-            <table>
-  <tr>
-    <td>&nbsp;PRODUCTOS</td>
-   
-    <td>Precio</td>
-    <td>Cantidad</td>
-    <td>Subtotal</td>
-    <td>Total</td>
-  </tr>
-  
-  <tr>
-    <td className="producto_celda">
-     
-<h5>Gloss lancome</h5>
-<img src="images/1957.webp" className="img_compra"/>
 
-    </td>
-    <td>16</td>
-    <td>1</td>
-    <td>16</td>
-    <td>16</td>
-  </tr>
-  <tr>
-    <td>Impuestos</td>
-    <td>17%</td>
-    <td></td>
-    <td>6.36</td>
-    <td>6.36</td>
-    
-  </tr>
-  <tr>
-    <td>Gastos de envío</td>
-    <td></td>
-    <td></td>
-    <td>3.36</td>
-    <td>3.36</td>
+  return (
+    <section className="container_pedido">
+      <div className="pedido_titulo">
+        <img src="/logo.jpg" className="logo_tienda1" />
+        <h1>TU PEDIDO</h1>
 
-    
-  </tr>
-  <tr>
-    <td>
-        <h3>Total</h3>
-    </td>
-   
-    <td></td>
+      </div>
+      <hr></hr>
 
-   
-    <td></td>
-    <td>15.72</td>
-    <td>15.72</td>
 
-    
-  </tr>
-</table>
-        </section>
-    )
+      <table>
+        <thead>
+          <tr>
+            <td>&nbsp;PRODUCTOS</td>
+
+            <td>Precio</td>
+            <td>Cantidad</td>
+            <td>Subtotal</td>
+            <td>Total</td>
+          </tr>
+        </thead>
+        <tbody>
+
+          {cart.map((item, index) => (
+
+            <tr key={index}>
+              <td className="producto_celda">
+
+                <h5>{item.name}</h5>
+                <img src={`/images/${item.image}`} className="img_compra" />
+
+              </td>
+              <td>16</td>
+              <td>1</td>
+              <td>16</td>
+              <td>16</td>
+            </tr>
+
+          ))}
+
+
+          <tr>
+            <td>Gastos de envío</td>
+            <td></td>
+            <td></td>
+            <td>3.36</td>
+            <td>3.36</td>
+
+          </tr>
+          <tr>
+            <td>
+              <h3>Total</h3>
+            </td>
+            <td></td>
+            <td></td>
+            <td>15.72</td>
+            <td>15.72</td>
+
+
+          </tr>
+        </tbody>
+      </table>
+
+    </section>
+  )
 }
