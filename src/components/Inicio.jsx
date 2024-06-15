c1a51e18f8fdd30b05fba6ca9ef96b8fee48f85d
81andie <133395577+81andie@users.noreply.github.com>
2024-06-15 13:04:26 +0200

    cambios en las secciones de sobreNosotros

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -5,12 +5,19 @@ export const Inicio = () => {
   return (
     <>
 
+
 <div className ="sobre_nosotrosPresentacion">
-      <h1>Sobre Nosotros</h1>
+  
+<h1>Sobre Nosotros</h1>
+  
+    
       {/*<iframe  src="images/MISS DIOR - THE NEW PARFUM.mp4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen className="iframe_sobrenosotros"></iframe>*/}
     
-    <span className="parr_sobreNosotros">Fundada en 1950, Cleopatra nació de la idea de crear una experiencia de compra única para los amantes de la belleza. Nos inspiramos en la naturaleza y la ciencia para seleccionar los mejores productos para ti.</span>
-   
+    <span>Fundada en 1910, Cleopatra nació de la idea de crear una experiencia de compra única para los amantes de la belleza. Nos inspiramos en la naturaleza y la ciencia para seleccionar los mejores productos para ti.</span>
+    <h1>Nuestra tienda</h1>
+    <span> Cleopatra, a sobrevivido en el tiempo, en las guerras y en épocas dónde hacerse un hueco en la supervivéncia, es una esencia de resistencia y resilencia. Una empresa familiar, dónde la 3a generación de la família toma las  riendas, hacia la modernidad, buscando las mejores exclusividades en todo tipo de marcas de lujo.</span>
+  <img src="https://img.freepik.com/fotos-premium/hay-tienda-monton-botellas-frascos-exhibicion-generativa-ai_1028863-43627.jpg?w=1060" className="img_perfumeria"/>
+
 
 </div>
    
