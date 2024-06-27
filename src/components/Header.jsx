37a232af7d7dc0e972a27f3b4483029f1156e6ef
81andie <133395577+81andie@users.noreply.github.com>
2024-06-27 10:54:24 +0200

    pruebas para implantar un buscador en la página web

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Navbar } from './Navbar';
 import { useState, useEffect } from 'react';
+import { OrderList } from 'primereact/orderlist';
+import { ProductServiceMarca } from '../photoservice/ProductServiceMarca';
+import { useParams } from 'react-router-dom';
 
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -8,6 +11,14 @@ import 'primeicons/primeicons.css';
 
 export const Header = () => {
     
+  const [products, setProducts ] = useState([]);
+
+  useEffect(() =>{
+
+   ProductServiceMarca.getProducts().then((data)=> setProducts(data));
+
+  }, []);
+ 
 
 
 
@@ -27,7 +38,9 @@ export const Header = () => {
     <header className="header">
         <img src="/logo.jpg" className="logo_tienda"/>
       
-        
+        <div className="card xl:flex xl:justify-content-right">
+            <OrderList value={products} onChange={(e) => setProducts(e.value)} header="Products" filter filterBy="name"></OrderList>
+        </div>
        
     
        
