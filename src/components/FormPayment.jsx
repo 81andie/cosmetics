f84536d955aa48e3d9403e4d01fc9ed8c6160098
81andie <133395577+81andie@users.noreply.github.com>
2024-07-07 18:25:45 +0200

    Acabando de hacer el responsive para la seccion payment

--- src/components/FormPayment.jsx
+++ src/components/FormPayment.jsx
@@ -150,23 +150,64 @@ export const FormPayment = () => {
 
                 </section>
 
+            </div>
+
+            <section className="carrusel_pedido">
+
+                <Carousel value={products} numVisible={3} numScroll={3} className="custom-carousel" circular
+                    autoplayInterval={3000} itemTemplate={productTemplate}
+
+                />
+
+                <img src="https://img.freepik.com/foto-gratis/gafas-sentado-portatil-credito-positivo_1262-2825.jpg?t=st=1720366720~exp=1720370320~hmac=c20f68c9490af04eb8f707dced9faec0fe6ed8555c02de85bb960a0adabb4011&w=360" className="img-carr-payment" />
+
+
+            </section>
+
+
+
+            <div className="Subscribir_titulo" >
+                <h1>Ahora si, <br />Casi ya estamos!! </h1>
+            </div>
+
+            <section className="subscripcion">
+
+
+                <img src="https://img.freepik.com/vector-premium/tarjeta-credito-bancaria-dos-caras-anverso-reverso-tarjeta-debito-realista-detallada-sobre-fondo-blanco-diseno-plantilla-tarjeta-credito-presentacion-simbolo-pago-dinero_93083-3211.jpg?w=360" />
+
+                <div className="container_subscripción">
+
+                    <h3>Intrudoduce los datos de tu tarjeta</h3>
+                    <h1>Por favor, inscribe tu nº de tarjeta, caducidad y Cod.seguridad</h1>
+
+                    <div className="form_container">
+                        <form>
+                            <label htmlFor="ftarget">Número de tarjeta:</label><br />
+                            <input type="number" id="fname" name="fname" placeholder="Tu nombre y apellidos" /><br />
+
+                            <label htmlFor="caducidad">Tu correo electrónico</label><br />
+                            <input type="date" id="data" name="data" placeholder="Data" /><br />
+                            {/*<input type="submit" value="Enviar" className="btnfos btnfos-1" />*/}
+
+                            <label htmlFor="fsegurity">Dirección:</label><br />
+                            <input type="number" id="fseguridad" name="fname" placeholder="Código de seguridad" /><br />
+                            <button className="enviar_btn"><a className="btnfos btnfos-1">
+                                <svg>
+                                    <rect x="0" y="0" fill="none" width="100%" height="100% " />
+
+                                </svg>
+                                Pagar
+                            </a>
+                            </button>
+                        </form>
+                    </div>
                 </div>
-
-                <section className="carrusel_pedido">
-
-                    <Carousel value={products} numVisible={3} numScroll={3} className="custom-carousel" circular
-                        autoplayInterval={3000} itemTemplate={productTemplate}
-
-                    />
-
-                    <img src="https://img.freepik.com/foto-gratis/blogger-alto-angulo-estudio-listo-filmar_23-2148360165.jpg?t=st=1720361188~exp=1720364788~hmac=61215e54c8b6e49e6bb5d5220ebcdcbb2f4fb1a222b2ad8e5fc813a377112fed&w=360" />
-
-
-                </section>
+            </section>
 
 
 
 
-            </>
-            )
+
+        </>
+    )
 }
